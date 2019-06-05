c47c2b1d11c8088fe900833917adaf6e43e603c0
Kaustubh Kanaujia <46839880+maniikk@users.noreply.github.com>
2019-06-05 13:56:00 +0530

    Unity Project

---
+++ Flappy Bird/Assets/Script/ColumnPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+    public int columnPoolsize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -2f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawned;
+	private float spawnXPosition=13f;
+    private int currentColumn = 0;
+	void Start ()
+    {
+        columns = new GameObject[columnPoolsize];
+        for (int i = 0; i < columnPoolsize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+        if(GameControl.instance.gameOver==false && timeSinceLastSpawned>=spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position=new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn = (currentColumn + 1) % columnPoolsize;
+        }
+	}
+}
---
+++ Flappy Bird/Assets/Script/GameControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public Text scoreText;
+    public float scrollSpeed = -1.5f;
+    private int score = 0;
+	void Awake ()
+    {
+		if(instance==null)
+        {
+            instance = this;
+        }
+        else if(instance !=null)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(gameOver==true&&Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+	}
+    public void BirdScored()
+    {
+        if (gameOver)
+            return;
+        else score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
